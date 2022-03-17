f63c07e849d1fe9b2cb1a4f2753ac9c50bb52e74
Tengiz Sharafiev <btolfa@gmail.com>
2022-03-17 19:00:15 +0300

    Add donation smart contract implementation

--- programs/donation/src/lib.rs
+++ programs/donation/src/lib.rs
@@ -1,15 +1,164 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke;
+use anchor_lang::solana_program::rent;
+use anchor_lang::solana_program::system_instruction;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod donation {
     use super::*;
+    use anchor_lang::solana_program::program::invoke_signed;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, authority: Pubkey) -> Result<()> {
+        let donation = &mut ctx.accounts.donation;
+        donation.authority = authority;
+        donation.bump = *ctx.bumps.get("donation").unwrap();
+        Ok(())
+    }
+
+    pub fn make_donation(ctx: Context<MakeDonation>, amount: u64) -> Result<()> {
+        require!(amount > 0, DonationError::InvalidAmount);
+
+        //transfer lamports to the donation bank
+        invoke(
+            &system_instruction::transfer(
+                &ctx.accounts.donor.key(),
+                &ctx.accounts.donation_bank.key(),
+                amount,
+            ),
+            &[
+                ctx.accounts.donor.to_account_info(),
+                ctx.accounts.donation_bank.to_account_info(),
+            ],
+        )?;
+
+        let registry = &mut ctx.accounts.registry;
+        if registry.amount == 0 {
+            registry.donor = ctx.accounts.donor.key();
+        }
+
+        // We are doing saturation add, because we can't decrease registry.amount
+        registry.amount = registry
+            .amount
+            .saturating_add(amount);
+
+        emit!(DonationEvent {
+            donation_bank: ctx.accounts.donation_bank.key(),
+            donor: ctx.accounts.donor.key(),
+            amount,
+        });
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
+        let rent_exempt = rent::Rent::get()?.minimum_balance(DonationBank::LEN);
+        let amount = ctx
+            .accounts
+            .donation_bank
+            .to_account_info()
+            .try_lamports()?
+            .saturating_sub(rent_exempt);
+
+        if amount > 0 {
+            let seeds = &[
+                ctx.accounts.donation_bank.authority.as_ref(),
+                &[ctx.accounts.donation_bank.bump],
+            ];
+            let signer = &[&seeds[..]];
+
+            invoke_signed(
+                &system_instruction::transfer(
+                    &ctx.accounts.donation_bank.key(),
+                    &ctx.accounts.destination.key(),
+                    amount,
+                ),
+                &[
+                    ctx.accounts.donation_bank.to_account_info(),
+                    ctx.accounts.destination.to_account_info(),
+                ],
+                signer,
+            )?;
+
+            emit!(WithdrawEvent {
+                donation_bank: ctx.accounts.donation_bank.key(),
+                destination: ctx.accounts.destination.key(),
+                amount,
+            });
+        }
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(authority: Pubkey)]
+pub struct Initialize<'info> {
+    #[account(init, payer = payer, space = DonationBank::LEN, seeds = [authority.as_ref()], bump)]
+    pub donation: Account<'info, DonationBank>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct MakeDonation<'info> {
+    #[account(mut)]
+    pub donation_bank: Account<'info, DonationBank>,
+    #[account(init_if_needed, payer = donor, seeds = [donation_bank.key().as_ref(), donor.key().as_ref()], bump)]
+    pub registry: Account<'info, Registry>,
+    #[account(mut)]
+    pub donor: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut, has_one = authority)]
+    pub donation_bank: Account<'info, DonationBank>,
+    pub authority: Signer<'info>,
+    #[account(mut, rent_exempt = enforce)]
+    /// CHECK: Checked above, used only for lamports adds
+    pub destination: UncheckedAccount<'info>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct DonationBank {
+    pub authority: Pubkey,
+    pub bump: u8,
+}
+
+impl DonationBank {
+    const LEN: usize = 32 + 1 + 8;
+}
+
+#[account]
+#[derive(Default)]
+pub struct Registry {
+    pub donor: Pubkey,
+    pub amount: u64,
+}
+
+#[error_code]
+pub enum DonationError {
+    #[msg("amount should be more than zero")]
+    InvalidAmount,
+    #[msg("Calculation failed due to overflow error")]
+    CalculationFailure,
+}
+
+#[event]
+pub struct DonationEvent {
+    pub donation_bank: Pubkey,
+    pub donor: Pubkey,
+    pub amount: u64,
+}
+
+#[event]
+pub struct WithdrawEvent {
+    pub donation_bank: Pubkey,
+    pub destination: Pubkey,
+    pub amount: u64,
+}
